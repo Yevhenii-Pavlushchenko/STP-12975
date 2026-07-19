211d4b5c4813d071fbd76a61356cb10113d808bc
JustFun <paw93@ukr.net>
2026-07-19 15:56:27 +0300

    cleared

--- src/main.js
+++ src/main.js
@@ -2,7 +2,4 @@ import './css/styles.css';
 
 import './js/swiper-bundle.min';
 import './js/burgerMenu';
-import './js/gameplay-swiper';
-import './js/feature-info-swiper';
-import './js/feature-details-swiper';
 import './js/reviews-swiper';
